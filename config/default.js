cb5326a8f678d6990f7b98fdda0847c7fe8691f5
Akinwale Ariwodola <akinwale@gmail.com>
2018-11-12 08:28:39 -0600

    add sequelize and mysql to packages

--- config/default.js
+++ config/default.js
@@ -1,4 +1,11 @@
 module.exports = {
   "apiPort": process.env.PORT || 3000,
-  "mongoUri": process.env.MONGO_URI || "mongodb://127.0.0.1:27017/abc"
+  "mongoUri": process.env.MONGO_URI || "mongodb://127.0.0.1:27017/abc",
+  "mysql": {
+    "host": process.env.MYSQL_HOST || "localhost",
+    "port": process.env.MYSQL_PORT || 3306,
+    "user": process.env.MYSQL_USER || "user",
+    "password": process.env.MYSQL_PASSWORD || "secret",
+    "database": process.env.MYSQL_DATABASE || "db"
+  }
 };
