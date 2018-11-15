642cae8d197150d5d6f9bd2ba0682574106697e4
Akinwale Ariwodola <akinwale@gmail.com>
2018-11-14 23:15:56 -0600

    basic sequelize controller crud

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,17 +1,62 @@
-const errors = require('./errors.js');
+const Task = require('../models').Task;
 
-exports.findAll = (req, res) => {
-  // Simulate task list, normally this would be retrieved from a database
-  const tasks = [
-    { _id: 1, name: 'milk' },
-    { _id: 2, name: 'cheese' },
-    { _id: 3, name: 'milk' },
-  ];
-
-  res.json(tasks);
+const create = (req, res) => {
+    const task = req.body;
+    Task.create(task).then(newTask => {
+        res.json({ taskId: newTask.id });
+    }).catch(error => {
+        res.status(400).json(error);
+    });
 };
 
-exports.buggyRoute = (req, res, next) => {
-  // Simulate a custom error
-  next(errors.newHttpError(400, 'bad request'));
+const update = (req, res) => {
+    const taskId = req.params.taskId;
+    Task.findById(taskId).then(task => {
+      if (!task) {
+        return res.status(404).json({ message: 'Task not found.' });
+      }
+
+      return task.update({
+        name: req.body.name,
+        isCompleted: req.body.isCompleted
+      }).then(task => res.status(200).send(task)).catch(error => res.status(500).json(error));
+    }).catch(error => res.status(500).json(error));
 };
+
+const deleteTask = (req, res) => {
+    const taskId = req.params.taskId;
+    Task.findById(taskId).then(task => {
+      if (!task) {
+        return res.status(404).json({ message: 'Task not found.' });
+      }
+
+      return task.destroy().
+        then(() => res.status(204).json()).
+        catch(error => res.status(500).json(error));
+
+    }).catch(error => res.status(500).json(error));
+};
+
+const getTasks = (req, res) => {
+  Task.findAll().then(tasks => res.status(200).json(tasks)).
+    catch(error => res.status(500).json(error));
+};
+
+const getTaskById = (req, res) => {
+  Task.findById(req.params.taskId).then(task => {
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    return res.status(200).json(task);
+  }).catch(error => res.status(500).json(error));
+};
+
+module.exports = {
+  create,
+  update,
+  deleteTask,
+  getTasks,
+  getTaskById
+};
+
